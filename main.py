c7625fb877eaf74ff8d5bc7cdb55993c1f0848e8
Matiasvaltteri <kivimatias04@gmail.com>
2026-01-06 14:16:11 +0200

    Implement core game mechanics: add player and asteroid classes, shooting functionality, and event logging

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+import sys
+
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import *
+from player import Player
+from logger import log_event, log_state
+from shot import Shot
+
 
 def main():
-    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
+                    log_event("asteroid_shot")
+            if asteroid.collides_with(player): 
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
